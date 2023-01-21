93ea649242f22a8626adb4ad8bc0ad5ec780812d
AhmadSalehEsfahani <48882325+AhmadSalehEsfahani@users.noreply.github.com>
2023-01-21 10:46:06 +0330

    Delete use-case directory

--- use-case/README.md
+++
@@ -1,4 +0,0 @@
-
-## dataset source (.csv file):
-
-Maudsley-Barton, S., & Yap, M. H. (2022). KINECAL (version 1.0.2). PhysioNet. https://doi.org/10.13026/ky1q-3h26.
--- use-case/toJson/main.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-
-field_names = ("valid", "part_id", "age", "BMI", "clinically-at-risk")
-df = pd.read_csv(input(".csv absolute file path: "), skipinitialspace=True, usecols=field_names)
-df["part_id"] = df["part_id"].str.extract('(\d+)', expand=False)
-df.to_json(input(".json absolute file path: "), orient='records', lines=True)
